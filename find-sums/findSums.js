a25ee4726f9a57579f8e261752ed249cb891aa3d
daniyal-abbassi <slimshady3753@protonmail.com>
2025-06-12 19:53:04 +0330

    chore: findSums problem initiate

---
+++ find-sums/findSums.js
@@ -0,0 +1,51 @@
+//given a list of numbers and a target number, find numbers that add-up together and equals with target.
+// NUMBERS SHOULD BE UNIQUE =>  [2,5,2,2,1] ?5 => ONLY ONE COMBINATION => 2,2,1
+
+const numbers = [10, 1, 2, 7, 6, 1, 5];
+const target = 8;
+const findCombinations = (candidates, target) => {
+    //sort the array
+    candidates.sort((a, b) => a - b)
+    //create a stack - combo(so far), remaining(sum needed) and start index
+    const results = [];
+    const stack = [
+        {
+            combo: [],
+            remaining: target,
+            startIndex: 0
+        }
+    ]
+    //start a loop
+    while (stack.length > 0) {
+
+        //get the current stack
+        const { combo, remaining, startIndex } = stack.pop();
+        //check if ramining is 0
+        if (remaining === 0) {
+            results.push(combo);
+            continue
+        }
+        //check if remaining is negetive
+        if (remaining < 0) {
+            continue
+        }
+
+        //go through next elements if candidates - for loop
+        for (let i = startIndex; i < candidates.length; i++) {
+
+            //check for duplicates
+            
+            //get the current number
+
+            //check current number if bigger then remaining
+
+            //add new stack to our main stack
+        }
+    }
+
+}
+
+
+
+
+//STACK APPROACH
